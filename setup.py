a36c63db0513e9a7da5f40d0a817a5f4b8040ec6
Ben Sigelman <bhs@lightstep.com>
2016-08-04 16:40:53 -0700

    Bump to version 2.1.3 since the tests were red

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightstep',
-    version='2.1.2',
+    version='2.1.3',
     description='LightStep Python OpenTracing Implementation',
     long_description='',
     author='LightStep',
