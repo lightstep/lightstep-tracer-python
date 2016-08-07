2fe413528c1bedf9294ab0ff47fa1f6efab1a970
Ben Sigelman <bhs@lightstep.com>
2016-08-07 08:02:46 -0400

    Bump version and depend on pytest

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightstep',
-    version='2.1.3',
+    version='2.1.4',
     description='LightStep Python OpenTracing Implementation',
     long_description='',
     author='LightStep',
     license='',
     install_requires=['thrift==0.9.2',
                       'jsonpickle',
+                      'pytest',
                       'basictracer>=2.1,<2.2',
                       'opentracing>=1.1,<1.2'],
     tests_require=['sphinx',
