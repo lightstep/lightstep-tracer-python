9c3e5d30b5a87a8763d9774ca07ac1e98db6b081
bcronin <bcronin@lightstep.com>
2016-09-01 13:07:43 -0700

    Updating Version to 2.1.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightstep',
-    version='2.1.9',
+    version='2.1.10',
     description='LightStep Python OpenTracing Implementation',
     long_description='',
     author='LightStep',
