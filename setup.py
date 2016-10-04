89cd61999eecbf7af4ce359ed7ed9cf3bc0f8b7a
Ben Sigelman <bhs@lightstep.com>
2016-10-04 09:45:24 -0700

    Updating Version to 3.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightstep',
-    version='3.0.0',
+    version='3.0.1',
     description='LightStep Python OpenTracing Implementation',
     long_description='',
     author='LightStep',
