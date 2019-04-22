4fcab8b4c332a6c8cd9c344af2c9d4f31ee23873
Cody Boppert <cody@codyboppert.nyc>
2019-04-22 16:55:43 -0400

    Do version update in separate pr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightstep',
-    version='4.0.4',
+    version='4.0.3',
     description='LightStep Python OpenTracing Implementation',
     long_description='',
     author='LightStep',
