4e1a91b81b28dd23261c92a52d3190c5bc421c19
Isaac Zinda <isaac@lightstep.com>
2019-08-21 16:34:31 -0700

    only check lightstep-benchmarks major version

--- ci/clone_lightstep_benchmarks.sh
+++ ci/clone_lightstep_benchmarks.sh
@@ -4,7 +4,6 @@ set -e
 
 REPO_URL="https://github.com/lightstep/lightstep-benchmarks.git"
 MAJOR_VERSION="0"
-MINOR_VERSION="1"
 
 # clone the repo
 git clone ${REPO_URL}
@@ -12,7 +11,7 @@ cd lightstep-benchmarks
 
 # checkout the newest commit whose tag matches the specified major / minor
 # version
-MATCHING_VERSIONS=`git tag --sort -version:refname --list "v${MAJOR_VERSION}.${MINOR_VERSION}.*"`
+MATCHING_VERSIONS=`git tag --sort -version:refname --list "v${MAJOR_VERSION}.*.*"`
 NEWEST_VERSION=`echo ${MATCHING_VERSIONS} | tr '\n' ',' | cut -d ',' -f 1`
 
 git checkout ${NEWEST_VERSION}
