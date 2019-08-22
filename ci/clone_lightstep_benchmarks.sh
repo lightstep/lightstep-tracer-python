85d92c5fae36fed2b19bd471df91526aefb3b594
Isaac Zinda <isaac@lightstep.com>
2019-08-22 09:26:41 -0700

    add some debugging info

--- ci/clone_lightstep_benchmarks.sh
+++ ci/clone_lightstep_benchmarks.sh
@@ -14,11 +14,14 @@ git fetch --all --tags
 
 # find most recent release whose tag matches the specified major / minor
 # version
-MATCHING_VERSIONS=`git tag --sort -version:refname --list "v${MAJOR_VERSION}.*.*"`
-NEWEST_VERSION=`echo ${MATCHING_VERSIONS} | tr '\n' ',' | cut -d ',' -f 1`
+MATCHING_VERSIONS=`git tag --sort -version:refname --list "v${MAJOR_VERSION}.*.*" | tr '\n' ','`
+NEWEST_VERSION=`echo ${MATCHING_VERSIONS} | cut -d ',' -f 1`
+
+echo "versions, newest to oldest: ${MATCHING_VERSIONS}"
+echo "newest version: ${NEWEST_VERSION}"
 
 # checkout the code corresponding to the newest tag
 git checkout tags/${NEWEST_VERSION}
 
 # so that we can confirm everything is a-okay
-git branch
+git branch; git tag
