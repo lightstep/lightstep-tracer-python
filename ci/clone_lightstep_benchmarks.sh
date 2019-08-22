b53e7f88ba1aad9148dff7fad1e399ead08cb1a1
Isaac Zinda <isaac@lightstep.com>
2019-08-22 08:50:39 -0700

    properly clone lightstep-benchmarks

--- ci/clone_lightstep_benchmarks.sh
+++ ci/clone_lightstep_benchmarks.sh
@@ -9,9 +9,16 @@ MAJOR_VERSION="0"
 git clone ${REPO_URL}
 cd lightstep-benchmarks
 
-# checkout the newest commit whose tag matches the specified major / minor
+# copy the tags to the local repo (from all remotes)
+git fetch --all --tags
+
+# find most recent release whose tag matches the specified major / minor
 # version
 MATCHING_VERSIONS=`git tag --sort -version:refname --list "v${MAJOR_VERSION}.*.*"`
 NEWEST_VERSION=`echo ${MATCHING_VERSIONS} | tr '\n' ',' | cut -d ',' -f 1`
 
-git checkout -b ${NEWEST_VERSION}
+# checkout the code corresponding to the newest tag
+git checkout tags/${NEWEST_VERSION}
+
+# so that we can confirm everything is a-okay
+git branch
